03143ff7bba9b7c50005adf5a84e405bb6904880
Jacob Drury <jacobdrury75@gmail.com>
2026-09-06 22:17:21 -0500

    Update .config/zsh/.zshrc Update .config/zsh/aliases.zsh Update .config/zsh/bindings.zsh Update .config/zsh/fzf.zsh Update .config/zsh/plugins.zsh Remove .config/zsh/prompt.zsh

---
+++ dot_config/zsh/empty_bindings.zsh
@@ -0,0 +1,14 @@
+zvm_after_init() {
+  # Ctrl+Right -> move forward one word (^[[1;5C is the terminal escape code)
+  bindkey '^[[1;5C' forward-word
+
+  # Ctrl+Left -> move backward one word (^[[1;5D is the terminal escape code)
+  bindkey '^[[1;5D' backward-word
+
+  # Ctrl+F -> fzf file picker (no hidden files)
+  bindkey '^F' _fzf_file_no_hidden
+
+  # Up/Down -> history search by substring (^[[A/^[[B are up/down arrow escape codes)
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
+}
